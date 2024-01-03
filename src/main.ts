fcbb994c8c435bab9b5c36a10b072b01f7c6847c
leemhoon00 <leemhoon000@gmail.com>
2024-01-03 22:49:18 +0900

    build: create Dockerfile

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://web.leemhoon00.com', 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+  });
   await app.listen(3000);
 }
 bootstrap();
